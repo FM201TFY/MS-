3f41acdbcd998942a51e44ab4554a87457b521b9
田富源 <132461194+FM201TFY@users.noreply.github.com>
2025-05-05 09:35:50 +0800

    Add files via upload

---
+++ 1_ACMEABA5050+20ACN_AC_Building.pl
@@ -0,0 +1,39 @@
+#!perl
+
+use strict;
+use Getopt::Long;
+use MaterialsScript qw(:all);
+
+my $acConstruction = Modules->AmorphousCell->Construction;
+
+for (my $n = 1; $n<21 ; $n++) {
+my $doc1 =  Documents->Import("library://20250416_differentACN_Dreiding/POLYMER/20ACN/20ACN  Script/$n.xsd");
+$acConstruction->AddComponent($doc1);
+$acConstruction->Loading($doc1) = 1;
+
+
+my $doc2 = Documents->Import("library://20250416_differentACN_Dreiding/POLYMER/ACM5050/create_ACM_EABA5050_dreiding  Script/$n.xsd");
+$acConstruction->AddComponent($doc2);
+$acConstruction->Loading($doc2) = 1;
+
+}
+
+my $results = $acConstruction->Run(Settings(
+	Temperature => 300, 
+	Quality => 'Medium',
+	OptimizeGeometry => 'Yes', 
+	Configurations => 1, 
+	TargetDensity => 1.2, 
+	LoadingMoves => 200, 
+	CheckCloseContacts => 'Yes', 
+	CloseContactvdWScale => 0.25, 
+	NumberBiasedTorsionSteps => 100, 
+	NumberBiasedHeadSegmentSteps => 100, 
+	LookAhead => 2, 
+	'3DPeriodicElectrostaticSummationMethod' => 'Atom based', 
+	CurrentForcefield => 'Dreiding',
+	AssignForcefieldTypes => 'Yes',
+	ChargeAssignment => 'Use current'));
+my $outTrajectory = $results->Trajectory;
+
+
---
+++ 1create_randompolymer.pl
@@ -0,0 +1,58 @@
+#!perl
+
+#use strict;
+use Getopt::Long;
+use MaterialsScript qw(:all);
+
+Tools->PolymerBuilder->ChangeSettings(Settings(
+	ForceConcentrations => 'Yes', 
+	UseProbabilities => 'No'));
+my $randomCopolymer = Tools->PolymerBuilder->RandomCopolymer;
+my $repeatUnit0 = Documents->Import("structures://repeat-units/acrylates/acrylonitrile.xsd");
+my $repeatUnit1 = $Documents{"NBR_Cis-budiene.xsd"};
+my $repeatUnit2 = $Documents{"NBR_Trans-budiene.xsd"};
+
+
+$randomCopolymer->ClearRepeatUnits();
+$randomCopolymer->AddRepeatUnit($repeatUnit0, 0.5, 0.5);
+$randomCopolymer->AddRepeatUnit($repeatUnit1);
+$randomCopolymer->AddRepeatUnit($repeatUnit2);
+
+
+$randomCopolymer->SetConcentration(0, 0.6);
+$randomCopolymer->SetConcentration(1, 0.08);
+$randomCopolymer->SetConcentration(2, 0.32);
+
+
+$randomCopolymer->SetReactivityRatio(0, 0, 1);
+$randomCopolymer->SetReactivityRatio(0, 1, 0.5);
+$randomCopolymer->SetReactivityRatio(0, 2, 0.5);
+$randomCopolymer->SetReactivityRatio(1, 0, 1);
+$randomCopolymer->SetReactivityRatio(1, 1, 1);
+$randomCopolymer->SetReactivityRatio(1, 2, 0.5);
+$randomCopolymer->SetReactivityRatio(2, 0, 0.5);
+$randomCopolymer->SetReactivityRatio(2, 1, 0.5);
+$randomCopolymer->SetReactivityRatio(2, 2, 1);
+
+#Enter the number of chains in the $chain_number
+for(my $chain_number = 10; $chain_number != 0; $chain_number = $chain_number-1)
+{
+
+#Enter the name of the folder at $doc
+my $doc = Documents-> New ("$chain_number.xsd");
+
+#For demo only - force the view to update
+$doc->UpdateViews;
+my $polymer = $randomCopolymer->Build($doc,10, , ,);
+
+#Geometry Optimization
+my $results = Modules->Forcite->GeometryOptimization->Run($doc, Settings(
+	Quality => 'Medium', 
+	CurrentForcefield => 'COMPASSIII', 
+	AssignForcefieldTypes => 'Yes', 
+	MaxIterations => 10000));
+
+};
+
+
+
